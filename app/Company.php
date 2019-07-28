6540f8f8c62acb01b9b5b0852944b0575aebadee
Shahid <m.shahidmuneer88@gmail.com>
2019-07-29 01:30:56 +0500

    Completed admin panel

---
+++ app/Company.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+class Company extends Model
+{
+    use Translatable;
+
+    protected $table="company";
+    protected $translatable = ['name'];
+
+    /**
+     * Statuses.
+     */
+    const STATUS_ACTIVE = 'ACTIVE';
+    const STATUS_INACTIVE = 'INACTIVE';
+
+    /**
+     * List of statuses.
+     *
+     * @var array
+     */
+    public static $statuses = [self::STATUS_ACTIVE, self::STATUS_INACTIVE];
+
+    protected $guarded = [];
+
+  
+
+    /**
+     * Scope a query to only include active pages.
+     *
+     * @param  $query  \Illuminate\Database\Eloquent\Builder
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('status', static::STATUS_ACTIVE);
+    }
+}
---
+++ app/Student.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+class Student extends Model
+{
+    use Translatable;
+
+    protected $table="students";
+    protected $fillable = ['name',"school_id","qr_code"];
+
+    /**
+     * Statuses.
+     */
+    const STATUS_ACTIVE = 'ACTIVE';
+    const STATUS_INACTIVE = 'INACTIVE';
+
+    /**
+     * List of statuses.
+     *
+     * @var array
+     */
+    public static $statuses = [self::STATUS_ACTIVE, self::STATUS_INACTIVE];
+
+    protected $guarded = [];
+
+
+    /**
+     * Scope a query to only include active pages.
+     *
+     * @param  $query  \Illuminate\Database\Eloquent\Builder
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('status', static::STATUS_ACTIVE);
+    }
+}
